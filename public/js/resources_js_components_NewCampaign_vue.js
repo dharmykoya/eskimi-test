d068f3dbd99179984222276b7c79e614bf369fc2
Damilola Adekoya <dharmykoya38@gmail.com>
2022-01-06 00:51:26 +0100

    last push

---
+++ public/js/resources_js_components_NewCampaign_vue.js
@@ -0,0 +1,415 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_components_NewCampaign_vue"],{
+
+/***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/NewCampaign.vue?vue&type=script&lang=js&":
+/*!******************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/NewCampaign.vue?vue&type=script&lang=js& ***!
+  \******************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  name: "NewCampaign"
+});
+
+/***/ }),
+
+/***/ "./resources/js/components/NewCampaign.vue":
+/*!*************************************************!*\
+  !*** ./resources/js/components/NewCampaign.vue ***!
+  \*************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _NewCampaign_vue_vue_type_template_id_d33efc96_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./NewCampaign.vue?vue&type=template&id=d33efc96&scoped=true& */ "./resources/js/components/NewCampaign.vue?vue&type=template&id=d33efc96&scoped=true&");
+/* harmony import */ var _NewCampaign_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./NewCampaign.vue?vue&type=script&lang=js& */ "./resources/js/components/NewCampaign.vue?vue&type=script&lang=js&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
+
+
+/* normalize component */
+;
+var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _NewCampaign_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _NewCampaign_vue_vue_type_template_id_d33efc96_scoped_true___WEBPACK_IMPORTED_MODULE_0__.render,
+  _NewCampaign_vue_vue_type_template_id_d33efc96_scoped_true___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns,
+  false,
+  null,
+  "d33efc96",
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "resources/js/components/NewCampaign.vue"
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/NewCampaign.vue?vue&type=script&lang=js&":
+/*!**************************************************************************!*\
+  !*** ./resources/js/components/NewCampaign.vue?vue&type=script&lang=js& ***!
+  \**************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_NewCampaign_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./NewCampaign.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/NewCampaign.vue?vue&type=script&lang=js&");
+ /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_NewCampaign_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/components/NewCampaign.vue?vue&type=template&id=d33efc96&scoped=true&":
+/*!********************************************************************************************!*\
+  !*** ./resources/js/components/NewCampaign.vue?vue&type=template&id=d33efc96&scoped=true& ***!
+  \********************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "render": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_NewCampaign_vue_vue_type_template_id_d33efc96_scoped_true___WEBPACK_IMPORTED_MODULE_0__.render),
+/* harmony export */   "staticRenderFns": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_NewCampaign_vue_vue_type_template_id_d33efc96_scoped_true___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns)
+/* harmony export */ });
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_NewCampaign_vue_vue_type_template_id_d33efc96_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./NewCampaign.vue?vue&type=template&id=d33efc96&scoped=true& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/NewCampaign.vue?vue&type=template&id=d33efc96&scoped=true&");
+
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/NewCampaign.vue?vue&type=template&id=d33efc96&scoped=true&":
+/*!***********************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/NewCampaign.vue?vue&type=template&id=d33efc96&scoped=true& ***!
+  \***********************************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "render": () => (/* binding */ render),
+/* harmony export */   "staticRenderFns": () => (/* binding */ staticRenderFns)
+/* harmony export */ });
+var render = function () {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c(
+    "div",
+    {
+      staticClass:
+        "min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-white",
+    },
+    [
+      _c("div", { staticClass: "sm:mx-auto sm:w-full sm:max-w-md" }, [
+        _c("img", {
+          staticClass: "mx-auto h-12 w-auto",
+          attrs: {
+            alt: "Workflow",
+            src: "https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg",
+          },
+        }),
+        _vm._v(" "),
+        _c(
+          "h2",
+          {
+            staticClass:
+              "mt-6 text-center text-3xl font-extrabold text-gray-900",
+          },
+          [_vm._v("\n            Sign in to your account\n        ")]
+        ),
+        _vm._v(" "),
+        _c("p", { staticClass: "mt-2 text-center text-sm text-gray-600" }, [
+          _vm._v(
+            "\n            Or\n            " + _vm._s(" ") + "\n            "
+          ),
+          _c(
+            "a",
+            {
+              staticClass: "font-medium text-indigo-600 hover:text-indigo-500",
+              attrs: { href: "#" },
+            },
+            [
+              _vm._v(
+                "\n                start your 14-day free trial\n            "
+              ),
+            ]
+          ),
+        ]),
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "mt-8 sm:mx-auto sm:w-full sm:max-w-md" }, [
+        _c(
+          "div",
+          { staticClass: "bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10" },
+          [
+            _c(
+              "form",
+              [
+                [
+                  _vm._m(0),
+                  _vm._v(" "),
+                  _vm._m(1),
+                  _vm._v(" "),
+                  _vm._m(2),
+                  _vm._v(" "),
+                  _vm._m(3),
+                  _vm._v(" "),
+                  _vm._m(4),
+                ],
+              ],
+              2
+            ),
+          ]
+        ),
+      ]),
+    ]
+  )
+}
+var staticRenderFns = [
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c(
+      "div",
+      {
+        staticClass:
+          "mb-8 border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600",
+      },
+      [
+        _c(
+          "label",
+          {
+            staticClass: "block text-xs font-medium text-gray-900",
+            attrs: { for: "name" },
+          },
+          [_vm._v("Name")]
+        ),
+        _vm._v(" "),
+        _c("input", {
+          staticClass:
+            "block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm",
+          attrs: {
+            id: "name",
+            name: "name",
+            placeholder: "Jane Doe",
+            type: "text",
+          },
+        }),
+      ]
+    )
+  },
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c(
+      "div",
+      {
+        staticClass:
+          "mb-8 border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600",
+      },
+      [
+        _c(
+          "label",
+          {
+            staticClass: "block text-xs font-medium text-gray-900",
+            attrs: { for: "daily_budget" },
+          },
+          [_vm._v("Daily\n                            Budget($USD)")]
+        ),
+        _vm._v(" "),
+        _c("input", {
+          staticClass:
+            "block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm",
+          attrs: {
+            id: "daily_budget",
+            name: "daily_budget",
+            placeholder: "900",
+            type: "number",
+          },
+        }),
+      ]
+    )
+  },
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c(
+      "div",
+      {
+        staticClass:
+          "mb-8 border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600",
+      },
+      [
+        _c(
+          "label",
+          {
+            staticClass: "block text-xs font-medium text-gray-900",
+            attrs: { for: "total_budget" },
+          },
+          [_vm._v("Total\n                            Budget($USD)")]
+        ),
+        _vm._v(" "),
+        _c("input", {
+          staticClass:
+            "block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm",
+          attrs: {
+            id: "total_budget",
+            name: "total_budget",
+            placeholder: "9000",
+            type: "number",
+          },
+        }),
+      ]
+    )
+  },
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c(
+      "div",
+      {
+        staticClass:
+          "mb-8 border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600",
+      },
+      [
+        _c(
+          "label",
+          {
+            staticClass: "block text-xs font-medium text-gray-900",
+            attrs: { for: "start_date" },
+          },
+          [_vm._v("Start Date")]
+        ),
+        _vm._v(" "),
+        _c("input", {
+          staticClass:
+            "block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm",
+          attrs: {
+            id: "start_date",
+            name: "start_date",
+            placeholder: "",
+            type: "date",
+          },
+        }),
+      ]
+    )
+  },
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c(
+      "div",
+      {
+        staticClass:
+          "mb-8 border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600",
+      },
+      [
+        _c(
+          "label",
+          {
+            staticClass: "block text-xs font-medium text-gray-900",
+            attrs: { for: "end_date" },
+          },
+          [_vm._v("End Date")]
+        ),
+        _vm._v(" "),
+        _c("input", {
+          staticClass:
+            "block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm",
+          attrs: {
+            id: "end_date",
+            name: "end_date",
+            placeholder: "",
+            type: "date",
+          },
+        }),
+      ]
+    )
+  },
+]
+render._withStripped = true
+
+
+
+/***/ })
+
+}]);
---
+++ resources/js/components/NewCampaign.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-white">
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+            <img alt="Workflow" class="mx-auto h-12 w-auto"
+                 src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"/>
+            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                Sign in to your account
+            </h2>
+            <p class="mt-2 text-center text-sm text-gray-600">
+                Or
+                {{ ' ' }}
+                <a class="font-medium text-indigo-600 hover:text-indigo-500" href="#">
+                    start your 14-day free trial
+                </a>
+            </p>
+        </div>
+
+        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <form>
+                    <template>
+                        <div
+                            class="mb-8 border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
+                            <label class="block text-xs font-medium text-gray-900" for="name">Name</label>
+                            <input class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm" id="name" name="name"
+                                   placeholder="Jane Doe"
+                                   type="text"/>
+                        </div>
+                        <div
+                            class="mb-8 border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
+                            <label class="block text-xs font-medium text-gray-900" for="daily_budget">Daily
+                                Budget($USD)</label>
+                            <input class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm" id="daily_budget" name="daily_budget"
+                                   placeholder="900"
+                                   type="number"/>
+                        </div>
+                        <div
+                            class="mb-8 border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
+                            <label class="block text-xs font-medium text-gray-900" for="total_budget">Total
+                                Budget($USD)</label>
+                            <input class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm" id="total_budget" name="total_budget"
+                                   placeholder="9000"
+                                   type="number"/>
+                        </div>
+                        <div
+                            class="mb-8 border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
+                            <label class="block text-xs font-medium text-gray-900" for="start_date">Start Date</label>
+                            <input class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm" id="start_date" name="start_date"
+                                   placeholder=""
+                                   type="date"/>
+                        </div>
+
+                        <div
+                            class="mb-8 border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
+                            <label class="block text-xs font-medium text-gray-900" for="end_date">End Date</label>
+                            <input class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm" id="end_date" name="end_date"
+                                   placeholder=""
+                                   type="date"/>
+                        </div>
+                    </template>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NewCampaign"
+    }
+</script>
+
+<style scoped>
+
+</style>
